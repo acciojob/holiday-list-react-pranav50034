fc84bd2b13b542eabf129823f9453ff42e30c58e
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-20 14:11:48 +0530

    great

---
+++ src/components/HolidayList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HolidayList = (props) => {
+   let k = 0;
+   return (
+      <>
+         <ol>
+            {props.city.map((city) => {
+               if (city.country === "India") {
+                  return <li key={"location" + ++k}>{city.name}</li>;
+               }
+            })}
+         </ol>
+      </>
+   );
+};
+
+export default HolidayList;
